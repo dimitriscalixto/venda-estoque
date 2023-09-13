a8e055e2c87c894b3d2c218374b7e83de45e6ff3
idimitrisc47 <dimitrisc47@gmail.com>
2023-09-13 18:16:14 -0500

    ajustes finais main.py

--- cadastro.py
+++ cadastro.py
@@ -14,54 +14,54 @@ class  Tela:
     def __init__(self,master):
 
         self.janela = master
-        self.janela.title("Formulário Cadastro")
-        self.janela.geometry('500x500')
-        self.heading = ttk.Label(self.janela,text="Cadastro de Produtos",font=('arial 20 bold'))
+        self.top_level = tk.Toplevel(self.janela)
+        self.top_level.geometry('500x500')
+        self.heading = ttk.Label(self.top_level,text="Cadastro de Produtos",font=('arial 20 bold'))
         self.heading.place(x=115,y=0)
 
-        self.nome_produto = ttk.Label(self.janela,text="Nome do Produto:",font=('arial 10'))
+        self.nome_produto = ttk.Label(self.top_level,text="Nome do Produto:",font=('arial 10'))
         self.nome_produto.place(x=15,y=70)
 
-        self.estoque = ttk.Label(self.janela, text="Estoque:", font=('arial 10'))
+        self.estoque = ttk.Label(self.top_level, text="Estoque:", font=('arial 10'))
         self.estoque.place(x=15, y=120)
 
-        self.preco_custo = ttk.Label(self.janela, text="Preco de Custo:", font=('arial 10'))
+        self.preco_custo = ttk.Label(self.top_level, text="Preco de Custo:", font=('arial 10'))
         self.preco_custo.place(x=15, y=170)
 
-        self.preco_venda = ttk.Label(self.janela, text="Preco de Venda:", font=('arial 10'))
+        self.preco_venda = ttk.Label(self.top_level, text="Preco de Venda:", font=('arial 10'))
         self.preco_venda.place(x=15, y=220)
 
-        self.nome_fornecedor = ttk.Label(self.janela, text="Nome do Fornecedor:", font=('arial 10'))
+        self.nome_fornecedor = ttk.Label(self.top_level, text="Nome do Fornecedor:", font=('arial 10'))
         self.nome_fornecedor.place(x=15, y=270)
 
-        self.tel_fornecedor = ttk.Label(self.janela, text="Telefone do Fornecedor:", font=('arial 10'))
+        self.tel_fornecedor = ttk.Label(self.top_level, text="Telefone do Fornecedor:", font=('arial 10'))
         self.tel_fornecedor.place(x=15, y=320)
 
-        self.id = ttk.Label(self.janela, text="ID:", font=('arial 10'))
+        self.id = ttk.Label(self.top_level, text="ID:", font=('arial 10'))
         self.id.place(x=15, y=370)
 
-        self.nome_entry = ttk.Entry(self.janela, width=20, font=('arial 10'))
+        self.nome_entry = ttk.Entry(self.top_level, width=20, font=('arial 10'))
         self.nome_entry.place(x=150,y=70)
 
-        self.estoque_entry = ttk.Entry(self.janela, width=20, font=('arial 10'))
+        self.estoque_entry = ttk.Entry(self.top_level, width=20, font=('arial 10'))
         self.estoque_entry.place(x=150, y=120)
 
-        self.preco_custo_entry = ttk.Entry(self.janela, width=20, font=('arial 10'))
+        self.preco_custo_entry = ttk.Entry(self.top_level, width=20, font=('arial 10'))
         self.preco_custo_entry.place(x=150, y=170)
 
-        self.preco_venda_entry = ttk.Entry(self.janela, width=20, font=('arial 10'))
+        self.preco_venda_entry = ttk.Entry(self.top_level, width=20, font=('arial 10'))
         self.preco_venda_entry.place(x=150, y=220)
 
-        self.nome_fornecedor_entry = ttk.Entry(self.janela, width=20, font=('arial 10'))
+        self.nome_fornecedor_entry = ttk.Entry(self.top_level, width=20, font=('arial 10'))
         self.nome_fornecedor_entry.place(x=150, y=270)
 
-        self.telefone_fornecedor_entry = ttk.Entry(self.janela, width=20, font=('arial 10'))
+        self.telefone_fornecedor_entry = ttk.Entry(self.top_level, width=20, font=('arial 10'))
         self.telefone_fornecedor_entry.place(x=160, y=320)
 
-        self.id_entry = ttk.Entry(self.janela, width=20, font=('arial 10'))
+        self.id_entry = ttk.Entry(self.top_level, width=20, font=('arial 10'))
         self.id_entry.place(x=35, y=370)
 
-        self.btn_add = ttk.Button(self.janela, width= 25, text="Cadastrar",command=self.get_items)
+        self.btn_add = ttk.Button(self.top_level, width= 25, text="Cadastrar",command=self.get_items)
         self.btn_add.place(x=120, y=420)
 
     def get_items(self):
@@ -88,8 +88,4 @@ class  Tela:
             con.close()
             tk.messagebox.showinfo("Aviso",message="Cadastro do Produto Realizado")
 
-
-master = ThemedTk(theme="breeze")
-app = Tela(master)
-master.mainloop()
 
